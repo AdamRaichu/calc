b17f5419fddff522436f5289012a9c911c7c62d3
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-13 09:53:22 -0400

    initial commit

---
+++ onload.js
@@ -0,0 +1,12 @@
+var myGraph = new Graph({
+    canvasId: "myCanvas",
+    minX: -10,
+    minY: -10,
+    maxX: 10,
+    maxY: 10,
+    unitsPerTick: 1
+  });
+
+//   myGraph.drawEquation(function(x) {
+    //  return f(x);
+//   }, "color", 3);
