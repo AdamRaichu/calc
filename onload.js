45e4cba5024bf40abb35b7ea3809eb12485d1533
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-14 09:31:56 -0400

    added auto-graphing of code in y= list

--- onload.js
+++ onload.js
@@ -1,12 +1,17 @@
 var myGraph = new Graph({
-    canvasId: "myCanvas",
-    minX: -10,
-    minY: -10,
-    maxX: 10,
-    maxY: 10,
-    unitsPerTick: 1
-  });
+  canvasId: "myCanvas",
+  minX: -10,
+  minY: -10,
+  maxX: 10,
+  maxY: 10,
+  unitsPerTick: 1
+});
 
-//   myGraph.drawEquation(function(x) {
-    //  return f(x);
-//   }, "color", 3);
+var fList = AR.$$("div.f span span[contenteditable=true]")
+var colors = AR.$$("div.f input[type=color]")
+
+for (c = 0; c < fList.length; c++) {
+  myGraph.drawEquation(function (x) {
+    return eval(fList[c].innerText);
+  }, colors[c], 3);
+}
